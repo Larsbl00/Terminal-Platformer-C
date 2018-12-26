8df4cc914b7cf2c5df53838b71df5cd9e8e69816
Larsbl00 <larsbloemers@gmail.com>
2018-12-26 13:43:42 +0100

    Created a header for the renderqueue

---
+++ src/RenderQueue.h
@@ -0,0 +1,19 @@
+/**
+ * @file RenderQueue.h
+ * @author Lars Bloemers
+ * @brief 
+ * @version 0.1
+ * @date 2018-12-26
+ * 
+ * @copyright Copyright (c) 2018
+ * 
+ */
+
+
+#include <Renderer.h>
+
+#ifndef RENDER_QUEUE_H
+#define RENDER_QUEUE_H
+
+
+#endif
