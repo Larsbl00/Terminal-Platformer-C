565c0427ac9ae39d7f76da40f817a07b7af80bbb
Larsbl00 <larsbloemers@gmail.com>
2018-12-31 15:08:27 +0100

    Created the struct and create for now

---
+++ src/Game.h
@@ -0,0 +1,43 @@
+/**
+ * @file Game.h
+ * @author Lars Bloemers
+ * @brief 
+ * @version 0.1
+ * @date 2018-12-31
+ * 
+ * @copyright Copyright (c) 2018
+ * 
+ */
+
+#ifndef GAME_H
+#define GAME_H
+
+
+#include "KeyReader.h"
+#include "Level.h"
+#include "RenderQueue.h"
+#include "RenderWindow.h"
+#include "Rectangle.h"
+#include "Player.h"
+
+typedef struct game
+{
+    level_t* level;
+    level_draw_parameter_t* level_draw_parameter;
+    player_t player;
+    player_draw_parameter_t player_draw_parameter;
+    render_queue_t render_queue;
+    render_window_t window;
+} game_t;
+
+/**
+ * @brief Creates a game
+ * 
+ * @param render_queue_size 
+ * @param window_height 
+ * @param window_width 
+ * @return game_t 
+ */
+game_t game_create(const size_t render_queue_size, const size_t window_height, const size_t window_width);
+
+#endif
